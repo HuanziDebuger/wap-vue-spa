299f6af6b5f2ff345f58d903a7d2b2061aecfd68
liuhuan <liuhuan-ds@gomeplus.com>
2017-08-09 12:59:06 +0800

    凑单页-领取赠品弹层

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@
 const Promise = require('bluebird')
 const webpack = require('webpack')
 const path = Promise.promisifyAll(require('path'))
-const fs   = Promise.promisifyAll(require('graceful-fs'))
+const fs = Promise.promisifyAll(require('graceful-fs'))
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
@@ -17,17 +17,15 @@ const fslist = require('ls-all')
 const isProd = process.env.NODE_ENV.match(/production/)
 const loaderConfig = require('./bin/loadersConfig.js')
 
-function webpackConfig ({ entries }) {
-    const config  = {
-        resolveLoader: {
-            moduleExtensions: ["-loader"]
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.vue$/,
-                    use:  [
-                        {
+function webpackConfig({ entries }) {
+    const config = {
+            resolveLoader: {
+                moduleExtensions: ["-loader"]
+            },
+            module: {
+                rules: [{
+                        test: /\.vue$/,
+                        use: [{
                             loader: "vue-loader",
                             options: {
                                 loaders: {
@@ -36,83 +34,76 @@ function webpackConfig ({ entries }) {
                                     css: loaderConfig.css
                                 }
                             }
-                        }
-                    ]
-                },
-                {
-                    test: /\.less$/,
-                    use: loaderConfig.less
-                },
-                {
-                    test: /\.css$/,
-                    use: loaderConfig.css
-                },
-                {
-                    test: /\.scss$/,
-                    use: loaderConfig.scss
-                },
-                {
-                    test: /\.ejs$|\.html$/, 
-                    use: [
-                        {
+                        }]
+                    },
+                    {
+                        test: /\.less$/,
+                        use: loaderConfig.less
+                    },
+                    {
+                        test: /\.css$/,
+                        use: loaderConfig.css
+                    },
+                    {
+                        test: /\.scss$/,
+                        use: loaderConfig.scss
+                    },
+                    {
+                        test: /\.ejs$|\.html$/,
+                        use: [{
                             loader: 'ejs-loader',
                             loader: 'ejs-compiled-loader'
-                        }
-                    ]
-                },
-                {
-                    test: /\.js$/,
-                    use:  loaderConfig.js,
-                },
-				{
-                    test: /^.*(gome-ui-kit).*?\.(png|jp[e]?g|bmp|gif)$/,
-                    use: [
-                        {
+                        }]
+                    },
+                    {
+                        test: /\.js$/,
+                        use: loaderConfig.js,
+                    },
+                    {
+                        test: /^.*(gome-ui-kit).*?\.(png|jp[e]?g|bmp|gif)$/,
+                        use: [{
                             loader: 'url-loader'
-                        }
-                    ]
-                },
-                {
-                    test: /^((?!(gome-ui-kit)).)*\.(png|jp[e]?g|bmp|gif)$/,
-                    use: [
-                        {
+                        }]
+                    },
+                    {
+                        test: /^((?!(gome-ui-kit)).)*\.(png|jp[e]?g|bmp|gif)$/,
+                        use: [{
                             loader: 'url-loader?name=[path]/[name].[ext]?v=[hash]&publicPath=//img.gomein.net.cn/plus/&outputPath=images/&limit=5000'
-                        }
-                    ]
-                }
+                        }]
+                    }
+                ]
+            },
+            plugins: [
+                new webpack.DllReferencePlugin({
+                    context: __dirname,
+                    manifest: require('./node_modules/gome-vue-vendor/gomeVueVendor-full-manifest.json')
+                }),
+                new webpack.DllReferencePlugin({
+                    context: __dirname,
+                    manifest: require('./node_modules/gome-vue-vendor/gomeVueVendor-manifest.json')
+                }),
+                new webpack.DllReferencePlugin({
+                    context: __dirname,
+                    manifest: require('./node_modules/gome-dll-utils/gomeDLLUtils-v1-manifest.json')
+                }),
+                new webpack.DllReferencePlugin({
+                    context: __dirname,
+                    manifest: require('./node_modules/gome-dll-bridge/gomeBridge-v1-manifest.json')
+                }),
+                new webpack.DllReferencePlugin({
+                    context: __dirname,
+                    manifest: require('./node_modules/gome-dll-ui-kit/gomeDLLUIKit-v1-manifest.json')
+                }),
             ]
-        },
-        plugins: [
-            new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require('./node_modules/gome-vue-vendor/gomeVueVendor-full-manifest.json')
-            }),
-            new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require('./node_modules/gome-vue-vendor/gomeVueVendor-manifest.json')
-            }),
-            new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require('./node_modules/gome-dll-utils/gomeDLLUtils-v1-manifest.json')
-            }),
-            new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require('./node_modules/gome-dll-bridge/gomeBridge-v1-manifest.json')
-            }),
-            new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require('./node_modules/gome-dll-ui-kit/gomeDLLUIKit-v1-manifest.json')
-            }),
-        ]
-    }
-    //配置入口
+        }
+        //配置入口
     config.entry = {}
-  
-    if(!isProd){
+
+    if (!isProd) {
         config.entry['hot'] = './lib/hotReloadEntry.js'
     }
-    for(var key in entries){
-        config.entry[key.replace(/\.js/,'')] = [entries[key]]
+    for (var key in entries) {
+        config.entry[key.replace(/\.js/, '')] = [entries[key]]
     }
     //配置出口
     config.output = {
@@ -121,34 +112,34 @@ function webpackConfig ({ entries }) {
         filename: `js/[name].js`,
     }
     config.devtool = 'module-source-map'
-		
-	config.plugins.push(new UglifyJSPlugin({
-		sourceMap: true,
-		comments: false
-	}))
-	
+
+    config.plugins.push(new UglifyJSPlugin({
+        sourceMap: true,
+        comments: false
+    }))
+
     //区别环境
-    if(!isProd){
-		config.plugins.push(new webpack.HotModuleReplacementPlugin())
-		config.plugins.push(new webpack.NoEmitOnErrorsPlugin())
-		config.plugins.push(new FriendlyErrorsPlugin())
-    }else{
-		config.plugins.push(new webpack.DefinePlugin({
-			'process.env':{
-				NODE_ENV: "'"+ `${process.env.NODE_ENV.trim()}` +"'"
-			}
-		}))
-		config.plugins.push(new ExtractTextPlugin({
-			filename: 'style/[name].css',
-			disable: false,
-			allChunks: true,
-		}))
-		config.plugins.push(new OptimizeCssAssetsPlugin({
-			cssProcessor: require('cssnano'),
-			cssProcessorOptions: { discardComments: {removeAll: true } },
-			canPrint: true
-		}))
-		
+    if (!isProd) {
+        config.plugins.push(new webpack.HotModuleReplacementPlugin())
+        config.plugins.push(new webpack.NoEmitOnErrorsPlugin())
+        config.plugins.push(new FriendlyErrorsPlugin())
+    } else {
+        config.plugins.push(new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: "'" + `${process.env.NODE_ENV.trim()}` + "'"
+            }
+        }))
+        config.plugins.push(new ExtractTextPlugin({
+            filename: 'style/[name].css',
+            disable: false,
+            allChunks: true,
+        }))
+        config.plugins.push(new OptimizeCssAssetsPlugin({
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: { discardComments: { removeAll: true } },
+            canPrint: true
+        }))
+
     }
     return config
 }
